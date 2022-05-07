9cb1b30e12b3731928fdf3899a8a9187cfbd85ae
mike97sj <33884813+mike97sj@users.noreply.github.com>
2022-05-07 13:54:41 -0400

    Add files via upload

---
+++ madlib and js_stuff/practice js.js
@@ -0,0 +1,19 @@
+//x = 0
+//i = 0
+
+//do {
+//   x = x + i;
+//  console.log(x)
+//   i++;
+//} while (i < 10);
+
+//for (let i = 1; i < 4; i += 1) {
+// console.log(i);
+//}
+
+
+const arrayOne = ["Bob", "Peter", "Patricia", "Nathaniel", "Bethany", "Jim", "Lathan", "GregoryJrTheThird", "Bill"]
+
+for (let i = 0; i < arrayOne.length; i++) {
+    console.log(arrayOne[i]);
+}
